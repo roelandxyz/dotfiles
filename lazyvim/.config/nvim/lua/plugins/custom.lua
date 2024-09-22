ff5524172678a08b8b4d54d12bde661cc39b0c5c
Roeland <mail@roeland.xyz>
2024-09-22 19:34:22 +0200

    lazyvim

---
+++ lazyvim/.config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,42 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+	-- add gruvbox
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+	-- Configure LazyVim to load gruvbox
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
+
+	-- add more treesitter parsers
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"css",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+		},
+	},
+}
