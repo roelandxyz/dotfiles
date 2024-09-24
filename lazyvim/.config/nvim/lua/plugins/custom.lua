e7af14ca98b2d0204be7cc293eebb3b31a3efb88
Roeland <mail@roeland.xyz>
2024-09-24 19:00:52 +0200

    disable autocomplete

--- lazyvim/.config/nvim/lua/plugins/custom.lua
+++ lazyvim/.config/nvim/lua/plugins/custom.lua
@@ -1,44 +1,48 @@
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-  { "folke/noice.nvim", enabled = false },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
+	-- { "folke/noice.nvim", enabled = false },
+	{ "akinsho/bufferline.nvim", enabled = false },
+	{ import = "lazyvim.plugins.extras.linting.eslint" },
+	{ import = "lazyvim.plugins.extras.formatting.prettier" },
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 
-  -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "css",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
-  },
+	-- manuel autocomplete (C-space)
+	{
+		"nvim-cmp",
+		opts = function(_, opts)
+			opts.completion = {
+				autocomplete = false,
+			}
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"css",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+		},
+	},
 }
