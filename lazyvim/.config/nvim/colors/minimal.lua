3f6e0d2a198b2a15f3a108b74a35a369c8f40cbb
Roeland <mail@roeland.xyz>
2024-09-25 16:41:21 +0200

    start minimal colors

---
+++ lazyvim/.config/nvim/colors/minimal.lua
@@ -0,0 +1,37 @@
+local hi = vim.api.nvim_set_hl
+
+local colors = {
+  base03 = "#002b36",
+  base02 = "#073642",
+  base01 = "#586e75",
+  base00 = "#657b83",
+  base0 = "#839496",
+  base1 = "#93a1a1",
+  base2 = "#eee8d5",
+  base3 = "#fdf6e3",
+  yellow = "#b58900",
+  orange = "#cb4b16",
+  red = "#dc322f",
+  magenta = "#d33682",
+  violet = "#6c71c4",
+  blue = "#268bd2",
+  cyan = "#2aa198",
+  green = "#859900",
+}
+
+hi(0, "Normal", { fg = colors.base00, bg = colors.base3 })
+hi(0, "Delimiter", { fg = colors.base00 })
+hi(0, "Type", { fg = colors.base00 })
+hi(0, "Constant", { fg = colors.base00 })
+hi(0, "Operator", { fg = colors.base00 })
+hi(0, "Comment", { fg = colors.base00 })
+hi(0, "Function", { fg = colors.base00 })
+hi(0, "Special", { fg = colors.base00 })
+hi(0, "String", { fg = colors.base00 })
+hi(0, "Identifier", { fg = colors.base00 })
+hi(0, "Statement", { fg = colors.base00 })
+hi(0, "@Variable", { fg = colors.base00 })
+
+-- hi(0, "@keyword.jsdoc", { fg = colors.base00 })
+hi(0, "@function.method.javascript", { fg = colors.base00, bold = true })
+hi(0, "@keyword.type.javascript", { fg = colors.base00, bold = true })
--- lazyvim/.config/nvim/lua/plugins/custom.lua
+++ lazyvim/.config/nvim/lua/plugins/custom.lua
@@ -1,48 +1,53 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
 
-	-- { "folke/noice.nvim", enabled = false },
-	{ "akinsho/bufferline.nvim", enabled = false },
-	{ import = "lazyvim.plugins.extras.linting.eslint" },
-	{ import = "lazyvim.plugins.extras.formatting.prettier" },
+  -- { "folke/noice.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { "mg979/vim-visual-multi" },
 
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "minimal",
+    },
+  },
 
-	-- manuel autocomplete (C-space)
-	{
-		"nvim-cmp",
-		opts = function(_, opts)
-			opts.completion = {
-				autocomplete = false,
-			}
-		end,
-	},
+  -- manuel autocomplete (C-space)
+  {
+    "nvim-cmp",
+    opts = function(_, opts)
+      opts.completion = {
+        autocomplete = false,
+      }
+    end,
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"html",
-				"javascript",
-				"css",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"tsx",
-				"typescript",
-				"vim",
-				"yaml",
-			},
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "css",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
 }
