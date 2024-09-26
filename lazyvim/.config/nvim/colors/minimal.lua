4097cd916eadc8e3a011319e322829075ed7bea8
Roeland <mail@roeland.xyz>
2024-09-26 14:09:40 +0200

    colors

--- lazyvim/.config/nvim/colors/minimal.lua
+++ lazyvim/.config/nvim/colors/minimal.lua
@@ -3,12 +3,12 @@ local hi = vim.api.nvim_set_hl
 local colors = {
   base03 = "#002b36",
   base02 = "#073642",
-  base01 = "#586e75",
+  emphasized = "#586e75",
   normal = "#657b83", --base00
-  base0 = "#839496",
-  base1 = "#93a1a1",
+  base0 = "#839496", --base0
+  base1 = "#93a1a1", --base1
   back_hightlight = "#eee8d5", --base2
-  normal_back = "#fdf6e3",
+  back_normal = "#fdf6e3", --base3
   yellow = "#b58900",
   orange = "#cb4b16",
   red = "#dc322f",
@@ -19,7 +19,7 @@ local colors = {
   green = "#859900",
 }
 
-hi(0, "Normal", { fg = colors.normal, bg = colors.normal_back })
+hi(0, "Normal", { fg = colors.normal, bg = colors.back_normal })
 hi(0, "Delimiter", { fg = colors.normal })
 hi(0, "Type", { fg = colors.normal })
 hi(0, "Constant", { fg = colors.normal })
@@ -29,10 +29,10 @@ hi(0, "Function", { fg = colors.normal })
 hi(0, "Special", { fg = colors.normal })
 hi(0, "String", { fg = colors.normal })
 hi(0, "Identifier", { fg = colors.normal })
-hi(0, "Statement", { fg = colors.normal })
+hi(0, "Statement", { fg = colors.emphasized })
 hi(0, "Visual", { bg = colors.back_hightlight })
 hi(0, "NonText", { fg = colors.back_hightlight })
-hi(0, "LspReferenceText", { bg = colors.back_hightlight, bold = true })
+hi(0, "LspReferenceText", { bg = colors.back_hightlight })
 
 -- In a terminal this may not work and you need to change your cursor in the terminal settings
 vim.opt.guicursor:append("a:Cursor/lCursor")
@@ -46,10 +46,11 @@ hi(0, "Added", { fg = colors.green })
 hi(0, "Removed", { fg = colors.red })
 hi(0, "Changed", { fg = colors.yellow })
 
--- hi(0, "Special", { fg = colors.normal, bg = colors.normal_back })
--- hi(0, "@spell", { fg = colors.normal, bg = colors.normal_back })
+-- hi(0, "Special", { fg = colors.normal, bg = colors.back_normal })
+-- hi(0, "@spell", { fg = colors.normal, bg = colors.back_normal })
 
 hi(0, "@Variable", { fg = colors.normal })
+hi(0, "@property", { fg = colors.normal })
 
 -- javascript
 hi(0, "@comment.documentation.javascript", { fg = colors.magenta })
@@ -57,11 +58,14 @@ hi(0, "@punctuation.delimiter.jsdoc", { fg = colors.magenta })
 hi(0, "@punctuation.bracket.jsdoc", { fg = colors.magenta, bold = true })
 hi(0, "@keyword.jsdoc", { fg = colors.magenta })
 hi(0, "@nospell.jsdoc", { fg = colors.magenta })
-hi(0, "@function.method.javascript", { fg = colors.normal, bold = true })
-hi(0, "@keyword.type.javascript", { fg = colors.normal, bold = true })
-hi(0, "@keyword.import.javascript", { fg = colors.normal, bold = true })
-
-hi(0, "@keyword.lua", { fg = colors.normal, bold = true })
+hi(0, "@function.method.javascript", { fg = colors.emphasized, bold = true })
+hi(0, "@keyword.type.javascript", { fg = colors.emphasized, bold = true })
+hi(0, "@keyword.coroutine.javascript", { fg = colors.green, bold = true })
+-- hi(0, "@keyword.javascript", { fg = colors.emphasized, bold = true })
+-- hi(0, "@keyword.conditional.javascript", { fg = colors.green })
+-- hi(0, "@keyword.return.javascript", { fg = colors.emphasized, bold = true })
+hi(0, "@variable.parameter.javascript", { fg = colors.blue })
+hi(0, "@keyword.import.javascript", { fg = colors.emphasized, bold = true })
 
 -- indent guides
 hi(0, "IblScope", { fg = colors.yellow })
