b4d0b6447fb631e5cb348abfa18db75370447db8
Roeland <mail@roeland.xyz>
2024-10-03 14:50:23 +0200

    tokyonight

--- lazyvim/.config/nvim/lua/plugins/custom.lua
+++ lazyvim/.config/nvim/lua/plugins/custom.lua
@@ -1,79 +1,16 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-
-      custom_highlights = function(colors)
-        return {
-          -- Comment = { fg = colors.flamingo },
-          ["@keyword.jsdoc"] = { fg = colors.green },
-          ["@comment.documentation.javascript"] = { fg = colors.green },
-          ["@punctuation.delimiter.jsdoc"] = { fg = colors.green },
-          ["@function.method.call.javascript"] = { fg = colors.text },
-          ["@variable.member.javascript"] = { fg = colors.text },
-          ["@number.javascript"] = { fg = colors.text },
-          ["@boolean.javascript"] = { fg = colors.text },
-          ["@function.call.javascript"] = { fg = colors.text },
-          ["@string.javascript"] = { fg = colors.rosewater },
-          -- ["@keyword.javascript"] = { fg = colors.subtext1 },
-          ["@variable.builtin.javascript"] = { fg = colors.lavender },
-        }
-      end,
-
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-  },
-
   { "folke/noice.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  -- tell LazyVim to stop messing with colorschemes
-  -- { "catppuccin/nvim", name = "catppuccin", enabled = false },
-  -- { "folke/tokyonight.nvim", enabled = false },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  -- { import = "lazyvim.plugins.extras.linting.eslint" },
+  -- { import = "lazyvim.plugins.extras.formatting.prettier" },
   { "mg979/vim-visual-multi" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin",
+      colorscheme = "tokyonight",
     },
   },
 
