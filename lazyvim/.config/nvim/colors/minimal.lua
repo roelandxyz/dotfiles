f7a8efe320ea8e592e0381ebb8e40f66fc5c60ef
Roeland <mail@roeland.xyz>
2024-11-09 20:54:20 +0100

    lazyvim colors

--- lazyvim/.config/nvim/colors/minimal.lua
+++ lazyvim/.config/nvim/colors/minimal.lua
@@ -1,20 +1,33 @@
 local hi = vim.api.nvim_set_hl
 
 local colors = {
-	keyword_prominent = "LightBlue",
-	keyword_noticeable = "Thistle",
-	functiondef = "NvimLightYellow",
+	keyword_prominent = "LightRed",
+	-- keyword_noticeable = "Honeydew",
+	keyword_noticeable = "LightYellow",
+	functiondef = "LightBlue",
 	normal = "NvimLightGrey2",
-	documentation = "DarkKhaki",
+	documentation = "NvimLightGrey4",
 }
 
 --- Reset to normal colors
-hi(0, "Normal", { fg = colors.normal, bg = "#0b1402" })
+hi(0, "Normal", { fg = colors.normal, bg = "#222421" })
 hi(0, "Function", { fg = colors.normal })
 hi(0, "Special", { fg = colors.normal })
 hi(0, "String", { fg = colors.normal })
 hi(0, "Identifier", { fg = colors.normal })
 hi(0, "Statement", { fg = colors.normal })
+
+-- To find the group names you can use :Inspect on your code
+-- Use :Telescope highlights
+-- Neovim colors (X11 rgb colors)
+-- https://github.com/neovim/neovim/blob/5331f87f6145f705c73c5c23f365cecb9fbc5067/src/nvim/highlight_group.c#L2937
+
+-- Go
+hi(0, "@function.method.go", { fg = colors.functiondef })
+hi(0, "@function.go", { fg = colors.functiondef })
+hi(0, "@type.definition.go", { fg = colors.keyword_prominent })
+hi(0, "@keyword.type.go", { fg = colors.keyword_noticeable })
+hi(0, "@keyword.function.go", { fg = colors.keyword_noticeable })
 
 --- Javascript
 hi(0, "@comment.documentation.javascript", { fg = colors.documentation })
@@ -25,15 +38,11 @@ hi(0, "@nospell.jsdoc", { fg = colors.documentation })
 hi(0, "@function.method.javascript", { fg = colors.functiondef })
 hi(0, "@function.javascript", { fg = colors.functiondef })
 hi(0, "@function.tsx", { fg = colors.functiondef })
-hi(0, "@keyword.type.javascript", { fg = colors.keyword_prominent })
-hi(0, "@keyword.function.javascript", { fg = colors.keyword_prominent })
+hi(0, "@keyword.type.javascript", { fg = colors.keyword_noticeable })
+hi(0, "@keyword.function.javascript", { fg = colors.keyword_noticeable })
 hi(0, "@keyword.coroutine.javascript", { fg = colors.keyword_noticeable })
--- hi(0, "@constructor.javascript", { fg = "NvimLightYellow", bold = true })
--- hi(0, "@variable.parameter.javascript", { fg = colors.blue })
--- hi(0, "@keyword.javascript", { fg = colors.emphasized, bold = true })
--- hi(0, "@keyword.conditional.javascript", { fg = colors.green })
--- hi(0, "@keyword.return.javascript", { fg = colors.emphasized, bold = true })
--- hi(0, "@keyword.import.javascript", { fg = colors.emphasized, bold = true })
+hi(0, "@lsp.typemod.class.declaration.javascript", { fg = colors.keyword_prominent })
+hi(0, "@lsp.typemod.property.declaration.javascript", { fg = colors.functiondef })
 
 --- Lua
 hi(0, "@comment.documentation.lua", { fg = colors.documentation })
--- lazyvim/.config/nvim/lua/plugins/custom.lua
+++ lazyvim/.config/nvim/lua/plugins/custom.lua
@@ -1,41 +1,42 @@
 return {
-  { "folke/noice.nvim", enabled = true },
+	{ "folke/noice.nvim", enabled = false },
 
-  -- no 'tabs'
-  { "akinsho/bufferline.nvim", enabled = false },
+	-- no 'tabs'
+	{ "akinsho/bufferline.nvim", enabled = false },
 
-  --  no indent guides
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "mg979/vim-visual-multi" },
+	--  no indent guides
+	{ "lukas-reineke/indent-blankline.nvim", enabled = false },
+	{ "mg979/vim-visual-multi" },
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "quiet",
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			-- colorscheme = "quiet",
+			colorscheme = "minimal",
+		},
+	},
 
-  -- manuel autocomplete (C-space)
-  {
-    "nvim-cmp",
-    opts = function(_, opts)
-      opts.completion = {
-        autocomplete = false,
-      }
-    end,
-  },
+	-- manuel autocomplete (C-space)
+	{
+		"nvim-cmp",
+		opts = function(_, opts)
+			opts.completion = {
+				autocomplete = false,
+			}
+		end,
+	},
 
-  -- Sort files by modified date
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
+	-- Sort files by modified date
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = {
 
-      pickers = {
-        find_files = {
-          find_command = { "rg", "--files", "--color", "never", "-g", "!.git", "--sortr=modified" },
-          hidden = true,
-        },
-      },
-    },
-  },
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--color", "never", "-g", "!.git", "--sortr=modified" },
+					hidden = true,
+				},
+			},
+		},
+	},
 }
